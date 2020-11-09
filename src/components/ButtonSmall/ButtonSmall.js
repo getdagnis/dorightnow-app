caee40e493ddb5d6205d6bfdf93ad2218d83b582
getdagnis <dagnis.skurbe@gmail.com>
2020-11-09 13:03:14 +0200

    string concat fix

--- src/components/ButtonSmall/ButtonSmall.js
+++ src/components/ButtonSmall/ButtonSmall.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./ButtonSmall.css";
 
 const ButtonSmall = (props) => {
-  const classList = "btn-small " + "btn-" + props.color;
+  const classList = "btn-small btn-" + props.color;
   return (
     <div>
       <div className={classList}>{props.title}</div>
