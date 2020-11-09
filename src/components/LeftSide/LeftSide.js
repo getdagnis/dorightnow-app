a23fb21f027e909193e9ac3c1ca59a9dc75b6652
getdagnis <dagnis.skurbe@gmail.com>
2020-11-09 12:52:32 +0200

    mobile test

--- src/components/LeftSide/LeftSide.js
+++ src/components/LeftSide/LeftSide.js
@@ -6,32 +6,32 @@ import "./LeftSide.css";
 
 export const LeftSide = () => {
   const tasks = [
-    // { id: 1, task: "do something", cat: "a" },
-    // { id: 2, task: "do nothing", cat: "b" },
-    // {
-    //   id: 3,
-    //   task:
-    //     "Don't eat too much bbq ribs. Finish the front-end of the do right now app, so that we can all finally do our tasks",
-    //   cat: 0,
-    // },
-    // {
-    //   id: 4,
-    //   task: "drink some wine, watch some movies, read a book and go to sleep",
-    //   cat: "c",
-    // },
-    // { id: 5, task: "do something", cat: "a" },
-    // {
-    //   id: 6,
-    //   task:
-    //     "Don't eat too much bbq ribs. Finish the front-end of the do right now app, so that we can all finally do our tasks",
-    //   cat: 0,
-    // },
+    { id: 1, task: "do something", cat: "a" },
+    { id: 2, task: "do nothing", cat: "b" },
+    {
+      id: 3,
+      task:
+        "Don't eat too much bbq ribs. Finish the front-end of the do right now app, so that we can all finally do our tasks",
+      cat: 0,
+    },
+    {
+      id: 4,
+      task: "drink some wine, watch some movies, read a book and go to sleep",
+      cat: "c",
+    },
+    { id: 5, task: "do something", cat: "a" },
+    {
+      id: 6,
+      task:
+        "Don't eat too much bbq ribs. Finish the front-end of the do right now app, so that we can all finally do our tasks",
+      cat: 0,
+    },
   ];
 
   return (
     <div className="left-side">
       <div className="left-side-top">
-        <h3 className="side-top-h3">do today</h3>
+        <h3 className="side-top-h3 title-drop">do today</h3>
         <ButtonSmall title="+ add new" color="red" />
       </div>
       <TaskList tasks={tasks} />
